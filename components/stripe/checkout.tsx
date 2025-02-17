2b219165548fe3e63ad5e83128e7814e2919354a
Sahil Ahmed <sahilahmed466@gmail.com>
2025-02-18 01:31:04 +0530

    feat: upgrade premium user after login

--- components/stripe/checkout.tsx
+++ components/stripe/checkout.tsx
@@ -1,17 +1,64 @@
 'use client'
 
-import React from 'react'
-import { Check, Lock } from 'lucide-react'
+import React, { useTransition } from 'react'
+import { usePathname } from 'next/navigation';
+
+import { loadStripe } from '@stripe/stripe-js'
+
+import { cn } from '@/lib/utils';
 import { useUser } from '@/lib/store/user'
-import Login from '../login';
+import { checkout } from '@/lib/actions/stripe';
+import { createClient } from '@/utils/supabase/client';
+
+import { Check, Lock } from 'lucide-react'
+import { FaGithub } from 'react-icons/fa';
 
 function Checkout() {
+  const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
   const user = useUser((state) => state.user);
+  
+    const supabase = createClient();
+  
+    const handleLogin = () => {
+      supabase.auth.signInWithOAuth({
+          provider: "github",
+          options: {
+              redirectTo: location.origin + "/auth/callback?next=" + pathname,
+          },
+      });
+    };
+
+  const handleCheckout = (e: any) => {
+    e.preventDefault();
+    startTransition(async () => {
+      const data = JSON.parse(
+        await checkout(user?.user_metadata?.email!, location.origin + pathname)
+      );
+
+      const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
+      await stripe?.redirectToCheckout({ sessionId: data.id });
+    })
+  };
 
   if (!user?.id) {
     return (
-      <div>
-        <Login />
+      <div className='w-full p-5 max-md:min-w-full max-w-[80%] flex flex-col items-center justify-center gap-6'>
+        <h2 className='text-xl lg:text-2xl text-center font-raleway font-medium'>
+          Create an account to read the full journal.
+        </h2>
+        <span className='space-y-1'>
+          <p className='text-center font-raleway font-normal text-xs lg:text-sm dark:text-neutral-300'>
+            The author made this journal available to CodeFusion members only.
+          </p>
+          <p className='font-raleway font-normal text-xs lg:text-sm text-center dark:text-neutral-300'>
+            If you're new to CodeFusion, create a new account to read this journal on us.
+          </p>
+        </span>
+        <button onClick={handleLogin} className='flex items-center justify-center gap-2 border rounded-md py-1.5 w-full md:w-1/2 font-inter text-sm bg-neutral-200/50 dark:bg-neutral-800/50 border-neutral-200 dark:border-neutral-700/50 hover:bg-neutral-100 dark:hover:bg-neutral-800 hover:border-neutral-200 dark:hover:border-neutral-700'>
+          <FaGithub />
+          Continue with Github
+        </button>
       </div>
     )
   }
@@ -61,7 +108,9 @@ function Checkout() {
       </div>
 
       {/** Upgrade btn */}
-      <div className='w-full max-md:min-w-full max-w-[80%] p-5 rounded-md bg-neutral-200/50 dark:bg-[#111]'>
+      <div className={cn('w-full max-md:min-w-full max-w-[80%] p-5 rounded-md bg-neutral-200/50 dark:bg-[#111]',
+        {'animate-pulse': isPending}    
+      )}>
         <div className='flex items-start gap-2 mb-3'>
           <p className='font-semibold font-poppins text-xl'>PRO</p>
           <span className='w-fit px-2 py-[2px] rounded-md text-center bg-white dark:bg-neutral-700'>
@@ -71,7 +120,7 @@ function Checkout() {
         <p className='text-xs font-outfit mb-5 text-[#111] dark:text-neutral-300'>
           For premium membership. Unlock all journal contents and code blocks.
         </p>
-        <button className='w-full text-center rounded-md py-1.5 font-inter text-sm border border-[hsl(155_78%_40%)] hover:opacity-90 bg-[hsl(151_67%_67%)] dark:bg-[hsl(155_100%_19%)] text-[#111] dark:text-[hsl(0_0%_98%)]'>
+        <button onClick={handleCheckout} className='w-full text-center rounded-md py-1.5 font-inter text-sm border border-[hsl(155_78%_40%)] hover:opacity-90 bg-[hsl(151_67%_67%)] dark:bg-[hsl(155_100%_19%)] text-[#111] dark:text-[hsl(0_0%_98%)]'>
           Upgrade now
         </button>
       </div>  
