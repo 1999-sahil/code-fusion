6c841a0291ec6b7e0ee461a637a3e9eb0fb680e1
Sahil Ahmed <sahilahmed466@gmail.com>
2025-03-01 21:24:39 +0530

    refactor: refactor the code

--- .source/index.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1740841283810"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"D:/githubRepos/development/codefusion/content/docs/index.mdx"}, data: docs_0 }], [])
