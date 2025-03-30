b214c0cd22e1918773f3aa1ba6bca085906c3c22
Sahil Ahmed <sahilahmed466@gmail.com>
2025-03-30 06:06:26 +0530

    fix: deployment pageProp error 11

--- app/dashboard/blog/edit/[id]/page.tsx
+++ app/dashboard/blog/edit/[id]/page.tsx
@@ -4,9 +4,13 @@ import { editBlogById } from '@/lib/actions/blog';
 import EditForm from './_components/edit-form';
 import { IBlogDetail } from '@/lib/types';
 
-async function page({ params }: { params: { id: string }}) {
+export type paramsType = Promise<{ id: string }>;
 
-  const { data: blog } = await editBlogById(params?.id);
+async function page(props: { params: paramsType }) {
+
+  const { id } = await props.params;
+
+  const { data: blog } = await editBlogById(id);
 
   return (
     <EditForm blog={blog as IBlogDetail} />
